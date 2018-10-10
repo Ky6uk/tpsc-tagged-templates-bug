c0f8ab35da7d4cf253489148ff5bb2ba24d3f582
Roman Nuritdinov (Ky6uk) <ky6uk.kun@gmail.com>
2018-10-10 18:17:11 +0300

    :rocket: Upgrade dependencies

--- index.tsx
+++ index.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+interface IBark {
+  color: string;
+}
+
 const Woof = styled.div<{ color?: string; }>`
+  color: #000;
+`;
+
+const Bark = styled.div<IBark>`
   color: #000;
 `;
 
 class Bork extends React.Component {
   render() {
     return (
-      <Woof color="black">
-        BORK-BORK-BORK
-      </Woof>
+      <>
+        <Bark color="red">
+          WOOF-WOOF-WOOF
+        </Bark>
+
+        <Woof color="black">
+          BORK-BORK-BORK
+        </Woof>
+      </>
     );
   }
 }
